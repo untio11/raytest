eb82c290d2be45e3e93e9af988a42c9f571f56cb
Robin Kneepkens <robin.kneepkens@hotmail.com>
2019-06-03 15:30:13 +0200

    Implementet MollerTrumbore

--- src/main/resources/triangleTracer.glsl
+++ src/main/resources/triangleTracer.glsl
@@ -7,6 +7,10 @@ layout(location = 0) uniform vec3 camera;
 layout(location = 1) uniform float fov;
 layout(location = 2) uniform mat3 transform;
 layout(location = 3) uniform int tringle_amount;
+uniform struct {
+    bool toggle;
+    vec3 location;
+} lights[3];
 
 layout(std430, binding = 1) buffer VertexPositions {
     vec4 parsed_positions[];
@@ -32,56 +36,44 @@ vec4 trace() {
     vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
     vec3 ray = getRay();
 
-    float tringle_area, ray_tringle_angle, d, t, u, v; // u and v can be used for interpolating
-    vec3 C, P; // P is the intersection point of the ray and the tringle, C is used to check if the intersection is inside
-    vec3 edge0, edge1, edge2; // edge0 (v0->v1) egde1 (v1->v2) edge2(v2->v0)
-    vec3 tringle_normal;
+    float u, v, t; // u and v are for interpolating, t is to find the intersection P
+    vec3 v01, v02; // two edges starting at v0
+    vec3 pvec, tvec, qvec; // Three stupid names
+    float det; // Determinant
 
     float closest = 1.0 / 0.0;
 
-    color = vec4(ray, 1.0);
-
     for (int i = 0; i < tringle_amount; i++) {
-        tringle_normal = cross(parsed_positions[(i * 3) + 1].xyz - parsed_positions[(i * 3) + 0].xyz,
-                               parsed_positions[(i * 3) + 2].xyz - parsed_positions[(i * 3) + 0].xyz);
+        v01 = parsed_positions[(i * 3) + 1].xyz - parsed_positions[(i * 3) + 0].xyz;
+        v02 = parsed_positions[(i * 3) + 2].xyz - parsed_positions[(i * 3) + 0].xyz;
+        pvec = cross(ray, v02);
+        det = dot(v01, pvec);
 
-        ray_tringle_angle = dot(tringle_normal, ray);
-
-        if (abs(ray_tringle_angle) <= 0.00001) { // Close enough to zero to count as parallel, so move on to the next one
+        if (abs(det) < 0.00001) { // Too close to parallel. If we remove abs(), we get backface culling
             continue;
         }
 
-        //tringle_area = length(tringle_normal) / 2.0; // Doesn't per say have to be divided by 2
-        d = dot(tringle_normal, parsed_positions[(i * 3) + 0].xyz); // Distance from origin to first vertex
-        t = - (dot(tringle_normal, camera) + d) / ray_tringle_angle;
-        if (t < 0.0) { // Tringle is behind the camera or not the closest tringle, so skip it
+        tvec = camera - parsed_positions[(i * 3) + 0].xyz;
+        u = dot(tvec, pvec) / det;
+        if (u < 0.0 || u > 1.0) {
             continue;
         }
 
-        P = camera + t * ray;
-
-        // Check with the first edge (v0->v1)
-        edge0 = parsed_positions[(i * 3) + 1].xyz - parsed_positions[(i * 3) + 0].xyz;
-        C = cross(edge0, (P - parsed_positions[(i * 3) + 0].xyz));
-        if (dot(tringle_normal, C) < 0.0) { // P is outside the tringle
+        qvec = cross(tvec, v01);
+        v = dot(ray, qvec) / det;
+        if (v < 0.0 || v + u > 1.0) {
             continue;
         }
 
-        // Check with the second edge (v1->v2)
-        edge1 = parsed_positions[(i * 3) + 2].xyz - parsed_positions[(i * 3) + 1].xyz;
-        C = cross(edge1, (P - parsed_positions[(i * 3) + 1].xyz));
-        if (dot(tringle_normal, C) < 0.0) { // P is outside the tringle
+        t = dot(v02, qvec) / det;
+        if (t >= closest) {
             continue;
         }
 
-        // Check with the last edge (v2->v0)
-        edge2 = parsed_positions[(i * 3) + 0].xyz - parsed_positions[(i * 3) + 2].xyz;
-        C = cross(edge2, (P - parsed_positions[(i * 3) + 2].xyz));
-        if (dot(tringle_normal, C) < 0.0) { // P is outside the tringle
-            continue;
-        }
+        closest = t;
+        //P = camera + t * ray;
 
-        color = vec4(1.0, 1.0, 1.0, 1.0);
+        color = vec4(0.5, 0.5, 0.5, 1.0);
     }
 
     return color;
